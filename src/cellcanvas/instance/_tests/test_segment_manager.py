6977afb7785686b66e166e3084a79dece8d91e39
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2024-03-05 17:24:06 +0100

    add viewer to tests

--- src/cellcanvas/instance/_tests/test_segment_manager.py
+++ src/cellcanvas/instance/_tests/test_segment_manager.py
@@ -7,20 +7,21 @@ from cellcanvas.instance.segment_manager import SegmentManager
 from cellcanvas.constants import PAINTABLE_KEY, CLASS_KEY, UNASSIGNED_CLASS
 
 
-def _make_labels_layer() -> Labels:
+def _make_labels_image() -> np.ndarray:
     label_image = np.zeros((10, 10, 10), dtype=int)
     label_image[0:5, 0:5, 0:5] = 1
     label_image[5:10, 5:10, 5:10] = 2
-    return Labels(label_image)
+    return label_image
 
 
-def test_feature_validation_no_features_table():
+def test_feature_validation_no_features_table(make_napari_viewer):
     """Check that all necessary columns in the feature table are added
     when they are not initially present.
     """
-    labels_layer = _make_labels_layer()
+    viewer = make_napari_viewer()
+    labels_layer = viewer.add_labels(_make_labels_image())
 
-    segment_manager = SegmentManager(labels_layer)
+    segment_manager = SegmentManager(labels_layer, viewer=viewer)
     features_table = labels_layer.features
 
     # check that the index column was added
@@ -39,21 +40,23 @@ def test_feature_validation_no_features_table():
     )
 
 
-def test_feature_validation_invalid_features_table():
+def test_feature_validation_invalid_features_table(make_napari_viewer):
     """Check that invalid features table raise an error."""
-    labels_layer = _make_labels_layer()
+    viewer = make_napari_viewer()
+    labels_layer = viewer.add_labels(_make_labels_image())
 
     labels_layer.features = pd.DataFrame({"not_index": [0, 1, 2]})
 
     with pytest.raises(ValueError):
         # if there is a features table, it should have an "index" column
         # with the instance IDs
-        _ = SegmentManager(labels_layer)
+        _ = SegmentManager(labels_layer, viewer=viewer)
 
 
-def test_feature_validation_valid_features_table():
+def test_feature_validation_valid_features_table(make_napari_viewer):
     """Check that valid features table raise is not overwritten."""
-    labels_layer = _make_labels_layer()
+    viewer = make_napari_viewer()
+    labels_layer = viewer.add_labels(_make_labels_image())
 
     paintable_values = [False, False, False]
     class_values = ["background", "hello", "hello"]
@@ -66,7 +69,7 @@ def test_feature_validation_valid_features_table():
 
     )
 
-    segment_manager = SegmentManager(labels_layer)
+    segment_manager = SegmentManager(labels_layer, viewer=viewer)
     features_table = labels_layer.features
 
     # check that the paintable values were not overwritten
@@ -82,9 +85,11 @@ def test_feature_validation_valid_features_table():
     )
 
 
-def test_paint_with_locked_features():
+def test_paint_with_locked_features(make_napari_viewer):
     """Check that paint only paints paintable features."""
-    labels_layer = _make_labels_layer()
+    viewer = make_napari_viewer()
+    labels_layer = viewer.add_labels(_make_labels_image())
+    initial_labels = labels_layer.data.copy()
 
     labels_layer.features = pd.DataFrame(
         {
@@ -94,7 +99,7 @@ def test_paint_with_locked_features():
         }
     )
 
-    _ = SegmentManager(labels_layer)
+    _ = SegmentManager(labels_layer, viewer=viewer)
 
     # lock paintable feature
     labels_layer.preserve_labels = True
@@ -107,7 +112,7 @@ def test_paint_with_locked_features():
 
     # check that the locked feature was not painted over
     np.testing.assert_array_equal(
-        labels_layer.data, _make_labels_layer().data
+        labels_layer.data, initial_labels
     )
 
     # paint over a paintable feature
@@ -117,9 +122,11 @@ def test_paint_with_locked_features():
     assert labels_layer.data[6, 6, 6] == 1, "The paint operation should have painted over the paintable feature"
 
 
-def test_fill_with_locked_features():
+def test_fill_with_locked_features(make_napari_viewer):
     """Check that fill only paints paintable features."""
-    labels_layer = _make_labels_layer()
+    viewer = make_napari_viewer()
+    labels_layer = viewer.add_labels(_make_labels_image())
+    initial_data = labels_layer.data.copy()
 
     labels_layer.features = pd.DataFrame(
         {
@@ -129,7 +136,7 @@ def test_fill_with_locked_features():
         }
     )
 
-    _ = SegmentManager(labels_layer)
+    _ = SegmentManager(labels_layer, viewer=viewer)
 
     # lock paintable feature
     labels_layer.preserve_labels = True
@@ -140,7 +147,7 @@ def test_fill_with_locked_features():
 
     # check that the locked feature was not painted over
     np.testing.assert_array_equal(
-        labels_layer.data, _make_labels_layer().data)
+        labels_layer.data, initial_data)
     
     # fill over a paintable feature
     labels_layer.fill((6, 6, 6), 1)
